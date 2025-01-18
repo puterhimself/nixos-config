adf016341111f4ab486da95f2165fc924d19aadc
puterhimself <22245429+puterhimself@users.noreply.github.com>
2025-01-18 17:29:59 +0530

    update

--- configuration.nix
+++ configuration.nix
@@ -119,8 +119,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
 	pkgs.neovim
 	pkgs.gh
 	pkgs.git
@@ -129,6 +127,7 @@
 	pkgs.gcc
 	pkgs.ghostty
 	pkgs.tmux
+	pkgs.yazi
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
